607ce806fa3402ab9c6f1e579e62bc768c7d910d
Tim Bushell <tcbushell@gmail.com>
2020-07-26 22:11:15 +0300

    modelMaker in space test

--- test/crispr.modelMaker-in-space.test.js
+++ test/crispr.modelMaker-in-space.test.js
@@ -16,69 +16,45 @@ for (let [model, tests] of Object.entries({
   Cosmos: {
     0: {},
     1: {},
-    2: {},
-    3: {},
   },
   Universe: {
     0: {},
-    1: {},
-    2: {},
-    3: {},
   },
   Galaxy: {
     0: {},
     1: {},
-    2: {},
-    3: {},
   },
   SolarSystem: {
     0: {},
     1: {},
-    2: {},
-    3: {},
   },
   Sun: {
     0: {},
     1: {},
-    2: {},
-    3: {},
   },
   Planet: {
     0: {},
     1: {},
-    2: {},
-    3: {},
   },
   Moon: {
     0: {},
-    1: {},
-    2: {},
-    3: {},
   },
   Asteroid: {
     0: {},
     1: {},
-    2: {},
-    3: {},
   },
   Satellite: {
     0: {},
-    1: {},
-    2: {},
-    3: {},
   },
   GPS: {
     0: {},
-    1: {},
-    2: {},
-    3: {},
   },
 })) {
   describe(`class | Crispr | ${model} modelMaker in space`, () => {
     for (let [depth, expectModelMade] of Object.entries(tests)) {
       it.only(`${model} at depth ${depth}`, () => {
         let modelsMined = this.crispr.modelMiner([model], depth)
-        let modelMade = this.crispr.modelMaker("Cosmos", modelsMined)
+        let modelMade = this.crispr.modelMaker(model, modelsMined)
         if (!this.jay.hasOwnProperty(model)) this.jay[model] = {}
         this.jay[model][depth] = modelMade
         modelMade.should.be.eql(expectModelMade)
